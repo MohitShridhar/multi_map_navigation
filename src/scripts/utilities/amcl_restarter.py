601f508e84bea64c21509c1b5972c5eeaa808182
Mohit Shridhar <mohit.appi@gmail.com>
2014-08-21 17:48:43 +0800

    Added rviz config file for navigation and map-settings

--- src/scripts/utilities/amcl_restarter.py
+++ src/scripts/utilities/amcl_restarter.py
@@ -40,11 +40,11 @@ def mapCb(msg):
     pose = cached_pose
     if (pose):
         pose = pose.pose.pose
-        rospy.set_param('sesto0/amcl/initial_pose_x', pose.position.x)
-        rospy.set_param('sesto0/amcl/initial_pose_y', pose.position.y)
+        rospy.set_param('amcl/initial_pose_x', pose.position.x)
+        rospy.set_param('amcl/initial_pose_y', pose.position.y)
         roll, pitch, yaw = tf.transformations.euler_from_quaternion( \
             [pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w])
-        rospy.set_param('sesto0/amcl/initial_pose_a', yaw)
+        rospy.set_param('amcl/initial_pose_a', yaw)
     proc = subprocess.Popen(["roslaunch", sys.argv[1]])
     shutdown = False
 
@@ -52,14 +52,14 @@ if (len(sys.argv) < 2):
     rospy.logerr("You must specify a roslaunch file as an argument.")
 else:
     proc = subprocess.Popen(["roslaunch", sys.argv[1]])
-    secondary = rospy.Subscriber("/sesto0/map", OccupancyGrid, mapCb)
-    pose_out = rospy.Publisher("/sesto0/amclinitialpose", PoseWithCovarianceStamped)
-    sub = rospy.Subscriber("/sesto0/initialpose", PoseWithCovarianceStamped, poseCb)
+    secondary = rospy.Subscriber("/map", OccupancyGrid, mapCb)
+    pose_out = rospy.Publisher("/amclinitialpose", PoseWithCovarianceStamped)
+    sub = rospy.Subscriber("/initialpose", PoseWithCovarianceStamped, poseCb)
     listener = tf.TransformListener(True, rospy.Duration(100))
     bad_counter = 0
     while not rospy.is_shutdown():
         try:
-            (trans,rot) = listener.lookupTransform('sesto0/map', 'sesto0/sesto_frame', rospy.Time())
+            (trans,rot) = listener.lookupTransform('map', 'base_link', rospy.Time())
             bad_counter = 0
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as ex:
             bad_counter = bad_counter + 1
